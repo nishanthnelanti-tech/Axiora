9e81fc8793c22a7301007fc7f6c64bab69aea864
Nishanth Kumar <nishanthnelanti@gmail.com>
2026-08-05 01:19:12 +0530

    implemented slice using redux toolkit store

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,12 +1,17 @@
 import { useEffect } from "react"
 import Home from "./pages/Home"
 import getCurrentUser from "./features/getCurrentuser.js"
+import { useDispatch } from "react-redux"
+import { setUserdata } from "./redux/userslice.js"
 
 const App = () => {
 
+  const dispatch=useDispatch()
+
   useEffect(()=>{
     const getUser=async()=>{
-      await getCurrentUser()
+      const data=await getCurrentUser()
+      dispatch(setUserdata(data))
     }
     getUser()
   },[])
--- Frontend/vite-project/src/features/getCurrentuser.js
+++ Frontend/vite-project/src/features/getCurrentuser.js
@@ -4,9 +4,11 @@ const getCurrentUser=async()=>{
     try{
         const {data}=await api.get("/api/me")
         console.log(data)
+        return data
     }
     catch(error){
         console.log(error)
+        return null
     }
 }
 
